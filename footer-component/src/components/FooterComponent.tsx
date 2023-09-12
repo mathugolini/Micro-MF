91ffc38d2b942549d8396769b529ecf689441fa7
Aline Vieira da Silva <aline.silva@darede.com.br>
2023-09-12 17:02:52 -0300

    feat: footer construction

---
+++ footer-component/src/components/FooterComponent.tsx
@@ -0,0 +1,25 @@
+const FooterComponent = () => {
+
+ 
+  return (
+
+    <footer className="bg-gray-900 text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+
+          © 2023 All rights reserved. Developed by Aline and Matheus.
+
+        </span>
+
+        <h1><span className="text-teal-400"></span></h1>
+
+      </div>
+
+    </footer>
+
+  );
+
+};
+
+ 
+
+export default FooterComponent;
